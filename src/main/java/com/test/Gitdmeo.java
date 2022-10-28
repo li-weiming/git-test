7d42d3a4651a6a53300c83c395a7dd91838c1e33
lwm <1626223478@qq.com>
2022-10-28 11:55:41 +0800

    master1 commit

--- src/main/java/com/test/Gitdmeo.java
+++ src/main/java/com/test/Gitdmeo.java
@@ -8,6 +8,8 @@ public class Gitdmeo {
         System.out.println("hello git333!");
         System.out.println("hello git444!");
         System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("master test!");
         System.out.println("master test!2222");
     }
 }
