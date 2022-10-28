98332fe38c0a1ce6f5bc7b2e11ec7f531747c416
lwm <1626223478@qq.com>
2022-10-28 11:17:12 +0800

    second commit

--- src/main/java/com/test/Gitdmeo.java
+++ src/main/java/com/test/Gitdmeo.java
@@ -4,5 +4,6 @@ package com.test;
 public class Gitdmeo {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2222!");
     }
 }
